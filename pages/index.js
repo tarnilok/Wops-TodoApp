55a5b26ae84cf28852cc7d645507b18ed320ee25
tarnilok <tarnilok@gmail.com>
2022-05-27 14:42:05 +0300

    user-agent recomposed

--- pages/index.js
+++ pages/index.js
@@ -22,8 +22,9 @@ export async function getServerSideProps() {
     const response = await fetch(process.env.END_POINT + "/api/todos", {
       headers: {
         'Content-Type': 'application/json',
-        Accept: 'application/json',
-        'User-Agent': '*',
+        "User-Agent":
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36", 
+      Accept: "application/json; charset=UTF-8",
       },
     });
     let todos = await response.json();
